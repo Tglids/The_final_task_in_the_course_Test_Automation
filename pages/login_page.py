057750154f0ed94815a58e56a2288b942333077f
Tohtamuradova L.G. Тохтамурадова Лидия <TLG@mikado.ru>
2023-09-08 14:35:05 +0300

    Реализована проверка наличия форм регистрации и логина на странице аутентификации, добавлены локаторы этих форм и осуществлена проверка наличия подстроки login в url

---
+++ pages/login_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        url = self.browser.current_url
+        assert 'login!!!' in url, 'Подстроки "login" нет в текущем url'
+
+    def should_be_login_form(self):
+        try:
+            self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
+        except:
+            assert False, 'Форма аутентификации не найдена'
+        assert True
+
+    def should_be_register_form(self):
+        try:
+            self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+        except:
+            assert False, 'Форма регистрации не найдена'
+        assert True
